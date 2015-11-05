c5afe517f6f0ec015d2b2a17a6d822fa64255241
Markus Koskinen <markus.koskinen@taisia.fi>
2015-11-05 23:47:02 +0200

    added a simple cleanup function for google storage backups

--- backup-mengpo-gs.bash
+++ backup-mengpo-gs.bash
@@ -24,6 +24,11 @@ PASSFILE="/root/backup-scripts/mengpo.pwd"
 # the backup upload process
 SNAPSIZE="3G"
 
+# Cleanup. How many snapshots you want to keep. 0 is infinite.
+SNAPSHOT_RETENTION_COUNT=4
+# More cleanup. Only remove files with this suffix. Set "" to rotate all.
+SNAPSHOT_ROTATION_SUFFIX="/*.dd.gz.gpg"
+
 # A directory withing the bucket
 REMOTEDIR="${VOLNAME}"
 # Arbitrary snapshot name, used for backup filename as well
@@ -36,7 +41,41 @@ GSUTIL=~/gsutil/gsutil
 # Do not edit below this line
 ##############################################
 
-# Create a snapshot (WARNING: currently set to 10G changes)
+# Cleanup starts here
+
+function snapshot_cleanup {
+  if [ $SNAPSHOT_RETENTION_COUNT -eq 0 ]
+  then
+    # If $SNAPSHOT_RETENTION_COUNT is set to 0, we don't "rotate"
+    return
+  fi
+
+  SNAPSHOT_LIST=$($GSUTIL ls gs://${BUCKET}/${REMOTEDIR}${SNAPSHOT_ROTATION_SUFFIX}|sort|uniq|sort)
+  SNAPSHOT_COUNT=$(echo "${SNAPSHOT_LIST}"|wc -l)
+
+  while [ $SNAPSHOT_COUNT -gt $SNAPSHOT_RETENTION_COUNT ]
+  do
+    echo "Snapshot count = $SNAPSHOT_COUNT"
+    REMOVEFILE=$(echo "${SNAPSHOT_LIST}"|head -n1)
+    echo "File to remove = ${REMOVEFILE}"
+
+    if ! $GSUTIL rm ${REMOVEFILE}; then
+      echo "ERROR: Could not perform snapshot cleanup. Check your permissions."
+      return
+    fi
+
+    SNAPSHOT_LIST=$($GSUTIL ls gs://${BUCKET}/${REMOTEDIR}|sort|uniq|sort)
+    SNAPSHOT_COUNT=$(echo "${SNAPSHOT_LIST}"|wc -l)
+  done
+}
+
+snapshot_cleanup
+
+#
+# Snapshot creation starts here
+#
+
+# Create a snapshot
 /usr/sbin/lvcreate -L${SNAPSIZE} -s -n "${SNAPNAME}" "/dev/${VOLGROUP}/${VOLNAME}"
 
 # DD the image through gzip and gsutil
